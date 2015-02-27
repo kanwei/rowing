5503ec97cabb6a15d22dcd7f3182422ff9589392
Kanwei Li <kanwei@gmail.com>
2015-02-27 14:53:49 -0500

    Initial

---
+++ project.clj
@@ -0,0 +1,34 @@
+(defproject mies "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2913"]]
+
+  :node-dependencies [[source-map-support "0.2.8"]]
+
+  :plugins [[lein-cljsbuild "1.0.5"]
+            [lein-npm "0.5.0"]]
+
+  :source-paths ["src" "target/classes"]
+
+  :clean-targets ["out" "out-adv"]
+
+  :cljsbuild {
+    :builds [{:id "dev"
+              :source-paths ["src"]
+              :compiler {
+                :main mies.core
+                :output-to "out/mies.js"
+                :output-dir "out"
+                :optimizations :none
+                :cache-analysis true
+                :source-map true}}
+             {:id "release"
+              :source-paths ["src"]
+              :compiler {
+                :main mies.core
+                :output-to "out-adv/mies.min.js"
+                :output-dir "out-adv"
+                :optimizations :advanced
+                :pretty-print false}}]})
